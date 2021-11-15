bd715ede4421341794f60d68e0c02add5f1b8069
HADI AMAN <hadiaman2.0@gmail.com>
2021-11-15 21:51:55 +0530

    DB_URI included

--- startup/config.js
+++ startup/config.js
@@ -4,4 +4,5 @@ module.exports = function(){
     if(!config.get('EMAIL_API')) throw new Error('EMAIL_API is not defined');
     if(!config.get('EMAIL')) throw new Error('EMAIL is not defined');
     if(!config.get('jswPrivateKey')) throw new Error('jswPrivateKey is not defined');
+    if(!config.get('DATABASE')) throw new Error('DB_URI is not defined');
 };
