7bbf22181c15c77048f4b15e42c12986d63c88db
HADI AMAN <hadiaman2.0@gmail.com>
2021-11-15 22:14:14 +0530

    ENV values

--- startup/database.js
+++ startup/database.js
@@ -1,20 +1,27 @@
 const {MongoClient} = require("mongodb");
-const config = require('../database/config.json')
+const config = require('../database/config.json');
+const conf = require('config');
 
 let database;
 
 module.exports = async function (){
-    const uri = `mongodb://localhost:27017`;
+    let uri;
+    if(conf.get('NODE_ENV')==='production')
+    {
+        uri = conf.get('DATABASE');
+    }else{
+        uri = `mongodb://localhost:27017`;
+    }
     const client = new MongoClient(uri);
 
     await client.connect()
         .then((data)=>{
-            console.log('Mongodb connection successful.')
-            database = data.db(config.DATABASE_NAME)
+            console.log('Mongodb connection successful.');
+            database = data.db(config.DATABASE_NAME);
         })
         .catch(()=>console.log('Mongodb connection failed.'));
 };
 
 module.exports.database = function (){
     return database;
-}
+};
